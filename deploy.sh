9832620f43015ed578903f75ee4f36a572a8ae67
martin mullins <martin.mullins942@gmail.com>
2019-04-29 12:43:08 +1000

    add deploy.sh4

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 
 repoPath=$(realpath $(dirname $0))
 echo "using repo path $repoPath"
-#find $repoPath/docs | grep -vi cname | xargs -I % git rm %
-#
-#cd frontend 
-#yarn build 
-#cd -
 
-#cp -vr $repoPath/frontend/build/* $repoPath/docs/
+find $repoPath/docs | grep -vi cname | xargs -I % git rm %
+
+cd frontend 
+yarn build 
+cd -
+
+cp -vr $repoPath/frontend/build/* $repoPath/docs/
 
 git add $repoPath/docs/*
